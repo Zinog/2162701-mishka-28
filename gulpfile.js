f531d2fd042dede1320f7dae67281afcc81760a8
Zinok <chevohnoe@mail.ru>
2023-05-14 13:46:19 +0300

    mishka

--- gulpfile.js
+++ gulpfile.js
@@ -123,7 +123,7 @@ const reload = (done) => {
 
 const watcher = () => {
   gulp.watch('source/sass/**/*.scss', gulp.series(styles));
-  gulp.watch('source/*.html', gulp.series (html, reload));
+  gulp.watch('source/*.html', gulp.series(html, reload));
   gulp.watch('source/js/*.js', gulp.series(scripts));
 }
 
